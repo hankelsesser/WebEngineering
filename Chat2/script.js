59ce7cefad0b62cb75913736bf87112a85692e2d
hankelsesser <63001993+hankelsesser@users.noreply.github.com>
2020-05-18 21:30:56 -0400

    1

--- Chat2/script.js
+++ Chat2/script.js
@@ -18,22 +18,21 @@ for (var i in receivedMessages) {
   displayedMessage.innerHTML = receivedMessages[i].content;
   receivedMessages.append(displayedMessage);
 }
-  // var time = new Date(message.timestamp);
-  // var a = [];
-  // a[0] = message.content;
-  // document.getElementById("display-messages").innerHTML += "<b>" + message.name + " - " + month + "/" + time.getDate() + ", " + time.getHours() + ":" + time.getMinutes() + "</b><br>" + a[0] + "<br><br>";
+  var time = new Date(message.timestamp);
+   var a = [];
+   a[0] = message.content;
+  document.getElementById("display-messages").innerHTML += "<b>" + message.name + " - " + month + "/" + time.getDate() + ", " + time.getHours() + ":" + time.getMinutes() + "</b><br>" + a[0] + "<br><br>";
 
-  // roomRef = database.ref('rooms/' + "_" + '/messages');
-  // roomRef.on('child_added', function(snapshot) {
-  //   // callbackData(snapshot.val());
-  //   var time = new Date(snapshot.val().timestamp);
-  //   var month = time.getMonth() + 1;
-  //   var timeDisplay = month + "/" + time.getDate() + ", " + time.getHours() + ":" + time.getMinutes();
-  //   var text = snapshot.val().content;
-  //   // var split = text.split();
-  //   // var testText = reverse.join();
-  //   // console.log(testText);
-  // });
+roomRef = database.ref('rooms/' + "_" + '/messages');
+ roomRef.on('child_added', function(snapshot) {
+   // callbackData(snapshot.val());
+ var time = new Date(snapshot.val().timestamp);
+ var month = time.getMonth() + 1;
+ var timeDisplay = month + "/" + time.getDate() + ", " + time.getHours() + ":" + time.getMinutes();
+var text = snapshot.val().content;
+// var testText = reverse.join();
+// console.log(testText);
+});
 }
 
 //The sendMessage('text') function will send a message from you to the database.
