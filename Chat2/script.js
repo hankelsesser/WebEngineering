ce84d2d15cca9904adc474e353775414cf1560ea
hankelsesser <63001993+hankelsesser@users.noreply.github.com>
2020-05-19 17:35:22 -0400

    time?

--- Chat2/script.js
+++ Chat2/script.js
@@ -11,30 +11,16 @@ function start(user) {
 //The message information is passed to you in the variable 'message'
 function recieveMessage(message) {
 	console.log(message);
-
-	// var time = new Date(message.timestamp);
-	var time = new Date(message.timestamp);
-	var month = time.getMonth() + 1;
-	var timeDisplay = month + "/" + time.getDate() + ", " + time.getHours() + ":" + time.getMinutes();
-	document.getElementById("display-messages").innerHTML += "<b>" + message.name + " - " + month + "/" + time.getDate() + ", " + time.getHours() + ":" + time.getMinutes() + "</b><br>" + message.content + "<br><br>";
+	document.getElementById("display-messages").innerHTML += "<b>" + message.name + ": " + message.content + "<br><br>";
 
 	roomRef = database.ref('rooms/_/');
   roomRef.on('value', function(snapshot) {
-		var time = new Date(snapshot.val().timestamp);
-		var month = time.getMonth() + 1;
-		var timeDisplay = month + "/" + time.getDate() + ", " + time.getHours() + ":" + time.getMinutes();
-		var text = snapshot.val().content;
 		var users = snapshot.val().messages;
 		console.log(users);
-
-		// var displayedMessages = document.getElementById("display-messages");
-		// displayedMessages.innerHTML = "<b>" + timeDisplay + "</b><br>" + message.content;
 
 		for (var i in users) {
 			var text1 = users[i].content;
 			console.log(text1);
-			// displayedMessages.innerHTML = "<b>" + name + " - " + timeDisplay + "</b><br>" + joinText;
-			// console.log(text[i]);
 		}
   });
 }
